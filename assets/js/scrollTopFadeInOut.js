28182a2838ef6a5e9b54bb5652e24300bdff2145
Stef Cruz <crzstefanie@gmail.com>
2020-08-03 10:50:21 +0100

    fix: navbar wasn't collapsing after click due to anchor links

--- assets/js/scrollTopFadeInOut.js
+++ assets/js/scrollTopFadeInOut.js
@@ -1,19 +1,4 @@
-// $(window).on("load",function() {
-//     $(window).scroll(function() {
-//       var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-//       $("#nav-top").each(function() {
-//         /* Check the location of each desired element */
-//         var objectBottom = $(this).offset().top + $(this).outerHeight();
-        
-//         /* If the element is completely within bounds of the window, fade it in */
-//         if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-//           if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-//         } else { //object goes out of view (scrolling up)
-//           if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-//         }
-//       });
-//     }).scroll(); //invoke scroll-handler on page-load
-//   });
+// Animated scroll to top button, taken from https://www.youtube.com/watch?v=Pd71ZZeIhaI
 
 window.addEventListener('scroll', function() {
     var scroll = document.querySelector('.nav-top');
